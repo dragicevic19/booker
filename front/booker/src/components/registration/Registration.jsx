c60dcf607691001dfec9fff4ee4d3974d2e256df
Marko Dragicevic <mdragicevic58@gmail.com>
2022-04-12 14:35:53 +0200

    2.4|#4 - uradjena validacija forme za registraciju na frontu

--- front/booker/src/components/registration/Registration.jsx
+++ front/booker/src/components/registration/Registration.jsx
@@ -1,5 +1,5 @@
 import React, { Component } from 'react'
-import Confirmation from '../Confirmation'
+import Confirmation from './Confirmation'
 import Success from './Success'
 import TypeOfUser from './TypeOfUser'
 import UserDetails from './UserDetails'
@@ -19,6 +19,20 @@ export default class Registration extends Component {
       street: '',
       phoneNumber: '',   
       explanation: '',
+
+      formErrors: {email: '', password: '', repPassword: '', firstName: '', lastName: '',
+                  country: '', city: '', street: '', phoneNumber: ''},
+      emailValid: false,
+      passwordValid: false,
+      repPasswordValid: false,
+      firstNameValid: false,
+      lastNameValid: false,
+      countryValid: false,
+      cityValid: false,
+      streetValid: false,
+      phoneNumberValid: false,
+
+      formValid: false
   }
 
   prevStep = () => {
@@ -36,14 +50,91 @@ export default class Registration extends Component {
   }
 
   handleChange = input => e => {
+    this.validateField(input, e.target.value)
     this.setState({ [input]: e.target.value })
   }
 
+  validateField = (fieldName, value) => {
+    let fieldValidationErrors = this.state.formErrors
+    let emailValid = this.state.emailValid
+    let passwordValid = this.state.passwordValid
+    let repPasswordValid = this.state.repPasswordValid
+    let firstNameValid = this.state.firstNameValid
+    let lastNameValid = this.state.lastNameValid
+    let countryValid = this.state.countryValid
+    let cityValid = this.state.cityValid
+    let streetValid = this.state.streetValid
+    let phoneNumberValid = this.state.phoneNumberValid
+
+    switch(fieldName) {
+      case 'email':
+        emailValid = value.match(/^([\w.%+-]+)@([\w-]+\.)+([\w]{2,})$/i);
+        fieldValidationErrors.email = emailValid ? '' : 'Email address is invalid'
+        break
+      case 'password':
+        passwordValid = value.length >= 8
+        fieldValidationErrors.password = passwordValid ? '' : 'must be at least 8 characters long'
+        break
+      case 'repPassword':
+          repPasswordValid = value === this.state.password
+          fieldValidationErrors.repPassword = repPasswordValid ? '' : 'passwords do not match'
+          break
+      case 'firstName':
+        firstNameValid = value.length > 0
+        fieldValidationErrors.firstName = firstNameValid ? '' : 'field cannot be empty'
+        break
+      case 'lastName':
+        lastNameValid = value.length > 0
+        fieldValidationErrors.lastName = lastNameValid ? '' : 'cannot be empty'
+        break
+      case 'country':
+        countryValid = value.length > 0
+        fieldValidationErrors.country = countryValid ? '' : 'cannot be empty'
+        break
+      case 'city':
+        cityValid = value.length > 0
+        fieldValidationErrors.city = cityValid ? '' : 'cannot be empty'
+        break
+      case 'street':
+        streetValid = value.length > 0
+        fieldValidationErrors.street = streetValid ? '' : 'cannot be empty'
+        break
+      case 'phoneNumber':
+        phoneNumberValid = value.match(/^(\+\d{1,2}\s)?\(?\d{3}\)?[\s.-]?\d{3}[\s.-]?\d{4}$/i)
+        fieldValidationErrors.phoneNumber = phoneNumberValid ? '' : 'invalid format'
+        break
+      
+      default:
+        break
+    }
+    this.setState({ 
+        formErrors: fieldValidationErrors,
+        emailValid: emailValid,
+        passwordValid: passwordValid,
+        repPasswordValid: repPasswordValid,
+        firstNameValid: firstNameValid,
+        lastNameValid: lastNameValid,
+        countryValid: countryValid,
+        cityValid: cityValid,
+        streetValid: streetValid,
+        phoneNumberValid: phoneNumberValid
+      }, this.validateForm)
+  } 
+
+  validateForm = () => {
+    this.setState({ formValid: this.state.emailValid && this.state.passwordValid && this.state.repPasswordValid && 
+                    this.state.firstNameValid && this.state.lastNameValid && this.state.countryValid && this.state.cityValid &&
+                    this.state.streetValid && this.state.phoneNumberValid })
+  }
 
   render() {
     const { step } = this.state
-    const { type, email, password, repPassword, firstName, lastName, birthday, country, city, street, phoneNumber, explanation } = this.state
-    const values = {type, email, password, repPassword, firstName, lastName, birthday, country, city, street, phoneNumber, explanation }
+    const { type, email, password, repPassword, firstName, lastName, birthday, country, city, street, phoneNumber, explanation,
+            formErrors, emailValid, passwordValid, repPasswordValid, firstNameValid, lastNameValid,
+            countryValid, cityValid, streetValid, phoneNumberValid, formValid } = this.state
+    const values = { type, email, password, repPassword, firstName, lastName, birthday, country, city, street, phoneNumber, explanation }
+    const validation = { formErrors, emailValid, passwordValid, repPasswordValid, firstNameValid, lastNameValid,
+      countryValid, cityValid, streetValid, phoneNumberValid, formValid }
 
     switch (step) {
       case 1:
@@ -62,6 +153,7 @@ export default class Registration extends Component {
            nextStep = { this.nextStep }
            handleChange = { this.handleChange }
            values = { values }
+           validation = { validation }
          />
         )
       case 3:
--- front/booker/src/components/registration/UserDetails.jsx
+++ front/booker/src/components/registration/UserDetails.jsx
@@ -1,6 +1,6 @@
 import { Button, TextField } from "@mui/material"
 
-const UserDetails = ({ prevStep, nextStep, handleChange, values }) => {
+const UserDetails = ({ prevStep, nextStep, handleChange, values, validation }) => {
   return (
     <div>
       <h1>Step 2 - Register as {values.type}</h1>
@@ -8,66 +8,90 @@ const UserDetails = ({ prevStep, nextStep, handleChange, values }) => {
 
         <div className="reg-first_name">
           <TextField
+            fullWidth
+            error={!validation.firstNameValid}
             required
             placeholder="First Name"
             label="First Name"
             onChange={handleChange('firstName')}
             defaultValue={values.firstName}
+            helperText={validation.firstNameValid ? '' : validation.formErrors.firstName}
           />
         </div>
 
         <div className="reg-last_name">
           <TextField
+            fullWidth
+            error={!validation.lastNameValid}
             required
             placeholder="Last Name"
             label="Last Name"
             onChange={handleChange('lastName')}
             defaultValue={values.lastName}
+            helperText={validation.lastNameValid ? '' : validation.formErrors.lastName}
+
           />
         </div>
 
         <div className="reg-country">
           <TextField
+            fullWidth
+            error={!validation.countryValid}
             required
             placeholder="Country"
             label="Country"
             onChange={handleChange('country')}
             defaultValue={values.country}
+            helperText={validation.countryValid ? '' : validation.formErrors.country}
+
           />
         </div>
 
         <div className="reg-city">
           <TextField
+            fullWidth
+            error={!validation.cityValid}
             required
             placeholder="City"
             label="City"
             onChange={handleChange('city')}
             defaultValue={values.city}
+            helperText={validation.cityValid ? '' : validation.formErrors.city}
           />
         </div>
 
         <div className="reg-street">
           <TextField
+            fullWidth
+            error={!validation.streetValid}
             required
             placeholder="Street"
             label="Street"
             onChange={handleChange('street')}
             defaultValue={values.street}
+            helperText={validation.streetValid ? '' : validation.formErrors.street}
+
           />
         </div>
 
         <div className="reg-email">
           <TextField
+            fullWidth
+            error={!validation.emailValid}
             required
             placeholder="Email Address"
             label="Email Address"
             onChange={handleChange('email')}
             defaultValue={values.email}
+            helperText={validation.emailValid ? '' : validation.formErrors.email}
+
           />
         </div>
 
         <div className="reg-password">
           <TextField
+            fullWidth
+            error={!validation.passwordValid}
             required
             placeholder="Password"
             label="Password"
@@ -75,11 +99,15 @@ const UserDetails = ({ prevStep, nextStep, handleChange, values }) => {
             autoComplete="current-password"
             onChange={handleChange('password')}
             defaultValue={values.password}
+            helperText={validation.passwordValid ? '' : validation.formErrors.password}
+
           />
         </div>
 
         <div className="reg-rep-password">
           <TextField
+            fullWidth
+            error={!validation.repPasswordValid}
             required
             placeholder="Repeat Password"
             label="Repeat Password"
@@ -87,25 +115,31 @@ const UserDetails = ({ prevStep, nextStep, handleChange, values }) => {
             autoComplete="current-password"
             onChange={handleChange('repPassword')}
             defaultValue={values.repPassword}
+            helperText={validation.repPasswordValid ? '' : validation.formErrors.repPassword}
+
           />
         </div>
 
         <div className="reg-phoneNum">
         <TextField
+            fullWidth
+            error={!validation.phoneNumberValid}
             required
             placeholder="Phone Number"
             label="Phone Number"
             onChange={handleChange('phoneNumber')}
             defaultValue={values.phoneNumber}
+            helperText={validation.phoneNumberValid ? '' : validation.formErrors.phoneNumber}
+
           />
         </div>
 
         { values.type !== 'Client' && 
           <div className="reg-explanation">
             <TextField
+              fullWidth
               label="Explanation of Registration"
               multiline
-              fullWidth
               rows={3}
               onChange={handleChange('explanation')}
               defaultValue={values.explanation}
@@ -121,8 +155,6 @@ const UserDetails = ({ prevStep, nextStep, handleChange, values }) => {
           }} 
           className="btnBackReg"
           variant="contained"
-          // color="error"
-          // disabled={btnDisabled}
           onClick={prevStep}
         >BACK
         </Button>
@@ -136,7 +168,7 @@ const UserDetails = ({ prevStep, nextStep, handleChange, values }) => {
           className="btnNextReg"
           variant="contained"
           color="success"
-          //disabled={btnDisabled}
+          disabled={!validation.formValid}
           onClick={nextStep}
         >NEXT
         </Button>
