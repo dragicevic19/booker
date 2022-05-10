9ce42be9b2603a3786359477fdb9ad0f2acd462e
Marko Dragicevic <mdragicevic58@gmail.com>
2022-05-10 20:54:49 +0200

    2.1.1 | 2 - validacija forme za unos nove vikendice

--- front/booker/src/components/addCottage/AddCottage.jsx
+++ front/booker/src/components/addCottage/AddCottage.jsx
@@ -13,6 +13,7 @@ const AddCottage = ({ user, values, validation, handleChange, handleImages }) =>
 			images.append("images", values.images[i])
 		}
 
+		//PRVI DEO: slanje samo slika
 		fetch('http://localhost:8080/api/upload', {
 			method: 'POST',
 			headers: {
@@ -24,6 +25,7 @@ const AddCottage = ({ user, values, validation, handleChange, handleImages }) =>
 				return res.json()
 			} else {throw Error('failed to save images')}
 
+		// DRUGI DEO: slanje svih ostalih podataka sa jedinstvenim nazivima slika koji su na beku
 		}).then(function(data) {
 			values.images = data
 			fetch('http://localhost:8080/api/add-cottage', {
@@ -59,93 +61,102 @@ const AddCottage = ({ user, values, validation, handleChange, handleImages }) =>
 							<TextField
 								fullWidth
 								required
+								error={ !validation.cottageNameValid.value && validation.cottageNameValid.enable }
 								placeholder="Cottage Name"
 								label="Cottage Name"
 								onChange={handleChange('cottageName')}
 								defaultValue={values.cottageName}
-								// helperText={ !validation.firstNameValid.value && validation.firstNameValid.enable ? validation.formErrors.firstName : ''}
+								helperText={ !validation.cottageNameValid.value && validation.cottageNameValid.enable ? validation.formErrors.cottageName : ''}
 							/>
             </div>
 						<div className='cottage-country'>
 							<TextField
 								fullWidth
 								required
+								error={ !validation.countryValid.value && validation.countryValid.enable }
 								placeholder="Country"
 								label="Country"
 								onChange={handleChange('country')}
 								defaultValue={values.country}
-								// helperText={ !validation.firstNameValid.value && validation.firstNameValid.enable ? validation.formErrors.firstName : ''}
+								helperText={ !validation.countryValid.value && validation.countryValid.enable ? validation.formErrors.country : ''}
 							/>
 						</div>
 						<div className='cottage-city'>
 							<TextField
 								fullWidth
 								required
+								error={ !validation.cityValid.value && validation.cityValid.enable }
 								placeholder="City"
 								label="City"
 								onChange={handleChange('city')}
 								defaultValue={values.city}
-								// helperText={ !validation.firstNameValid.value && validation.firstNameValid.enable ? validation.formErrors.firstName : ''}
+								helperText={ !validation.cityValid.value && validation.cityValid.enable ? validation.formErrors.city : ''}
 							/>
 						</div>
 						<div className='cottage-street'>
 							<TextField
 								fullWidth
 								required
+								error={ !validation.streetValid.value && validation.streetValid.enable }
 								placeholder="Street"
 								label="Street"
 								onChange={handleChange('street')}
 								defaultValue={values.street}
-								// helperText={ !validation.firstNameValid.value && validation.firstNameValid.enable ? validation.formErrors.firstName : ''}
+								helperText={ !validation.streetValid.value && validation.streetValid.enable ? validation.formErrors.street : ''}
 							/>
 						</div>
 						<div className='cottage-rooms'>
 							<TextField
 								fullWidth
 								required
-								type="number"
+								error={ !validation.numOfRoomsValid.value && validation.numOfRoomsValid.enable }
+								// type="number"
 								placeholder="Number of Rooms"
 								label="Number of Rooms"
 								onChange={handleChange('numOfRooms')}
 								defaultValue={values.numOfRooms}
 								// InputLabelProps={{shrink: true}}
-								// helperText={ !validation.firstNameValid.value && validation.firstNameValid.enable ? validation.formErrors.firstName : ''}
+								helperText={ !validation.numOfRoomsValid.value && validation.numOfRoomsValid.enable ? validation.formErrors.numOfRooms : ''}
 							/>
 						</div>
 						<div className='cottage-capacity'>
 							<TextField
 								fullWidth
 								required
-								type="number"
+								error={ !validation.capacityValid.value && validation.capacityValid.enable }
+								// type="number"
 								placeholder="Capacity"
 								label="Capacity"
 								onChange={handleChange('capacity')}
 								defaultValue={values.capacity}
-								// helperText={ !validation.firstNameValid.value && validation.firstNameValid.enable ? validation.formErrors.firstName : ''}
+								helperText={ !validation.capacityValid.value && validation.capacityValid.enable ? validation.formErrors.capacity: ''}
 							/>
 						</div>
 						<div className='cottage-price'>
 							<TextField
 								fullWidth
 								required
-								type="number"
+								error={ !validation.priceValid.value && validation.priceValid.enable }
+								// type="number"
+								// value={values.price}
 								placeholder="Price per Night [€]"
 								label="Price per Night [€]"
 								onChange={handleChange('price')}
 								defaultValue={values.price}
-								// helperText={ !validation.firstNameValid.value && validation.firstNameValid.enable ? validation.formErrors.firstName : ''}
+								helperText={ !validation.priceValid.value && validation.priceValid.enable ? validation.formErrors.price : ''}
 							/>
 						</div>
 						<div className='cottage-fee'>
 							<TextField
 								fullWidth
 								required
-								type="number"
+								error={ !validation.feeValid.value && validation.feeValid.enable }
+								// type="number"
 								placeholder="Cancellation Fee[€]"
 								label="Cancellation Fee[€]"
 								onChange={handleChange('fee')}
 								defaultValue={values.fee}
-								// helperText={ !validation.firstNameValid.value && validation.firstNameValid.enable ? validation.formErrors.firstName : ''}
+								helperText={ !validation.feeValid.value && validation.feeValid.enable ? validation.formErrors.fee : ''}
 							/>
 						</div>
 
@@ -175,7 +186,6 @@ const AddCottage = ({ user, values, validation, handleChange, handleImages }) =>
 							Select images:<br/>
 							<input 
 								type="file" 
-								// inputProps={{ multiple: true }} 
 								accept="image/*"
 								multiple
 								onChange={handleImages('images')}
@@ -204,7 +214,7 @@ const AddCottage = ({ user, values, validation, handleChange, handleImages }) =>
 							className="btnNext"
 							variant="contained"
 							color="success"
-							// disabled={!validation.formValid}
+							disabled={!validation.formValid}
 							onClick={addBtnClick}
 						>ADD
 						</Button>
--- front/booker/src/components/addCottage/NewCottage.jsx
+++ front/booker/src/components/addCottage/NewCottage.jsx
@@ -2,7 +2,6 @@ import { Component } from "react"
 import AddCottage from "./AddCottage"
 
 export default class NewCottage extends Component {
-// this.props.user
     constructor(props) {
         super(props)
         this.state = {
@@ -18,10 +17,26 @@ export default class NewCottage extends Component {
             fee: '',
             images: '',
             // dodatne usluge,
+
+            formErrors: {
+                cottageName: '', country: '', city: '', street: '', numOfRooms: '', 
+                capacity: '', price: '', fee: ''
+            },
+            cottageNameValid: {enable: false, value: false},
+            countryValid: {enable: false, value: false},
+            cityValid: {enable: false, value: false},
+            streetValid: {enable: false, value: false},
+            numOfRoomsValid: {enable: false, value: false},
+            capacityValid: {enable: false, value: false},
+            priceValid: {enable: false, value: false},
+            feeValid: {enable: false, value: false},
+
+            formValid: false,
         }
     }
 
     handleChange = input => e => {
+        this.validateField(input, e.target.value)
         this.setState({ [input]: e.target.value })
     }
 
@@ -29,16 +44,114 @@ export default class NewCottage extends Component {
         this.setState({[input]: e.target.files})
     }
 
-    render() {
-        const { cottageName, country, city, street, description, numOfRooms, capacity, regulations, price, fee, images } = this.state
-        const values = {cottageName, country, city, street, description, numOfRooms, capacity, regulations, price, fee, images }
-        return(
-            <AddCottage
-                user={ this.props.user }
-                values={ values }
-                handleChange = {this.handleChange}
-                handleImages = {this.handleImages}
-            />
-        )
+    validateField = (fieldName, value) => {
+        let fieldValidationErrors = this.state.formErrors
+        let cottageNameValid = this.state.cottageNameValid
+        let countryValid = this.state.countryValid
+        let cityValid = this.state.cityValid
+        let streetValid = this.state.streetValid
+        let numOfRoomsValid = this.state.numOfRoomsValid
+        let capacityValid = this.state.capacityValid
+        let priceValid = this.state.priceValid
+        let feeValid = this.state.feeValid
+
+        switch(fieldName) {
+					case 'cottageName':
+						value = value.trim()
+						cottageNameValid.enable = true
+						cottageNameValid.value = value.length > 0
+						fieldValidationErrors.cottageName = cottageNameValid.value ? '' : 'Field cannot be empty'
+						break
+
+					case 'country':
+						value = value.trim();
+						countryValid.enable = true
+						countryValid.value = value.match(/^[a-zA-Z]+(?:[\s-][a-zA-Z]+)*$/i)
+						if (!countryValid.value) {
+							fieldValidationErrors.country = countryValid.value ? '' : 'Invalid input'
+							break
+						}
+						countryValid.value = value.length > 0
+						fieldValidationErrors.country = countryValid.value ? '' : 'Field cannot be empty'
+						break
+					case 'city':
+						value = value.trim();
+						cityValid.enable = true
+						cityValid.value = value.match(/^[a-zA-Z]+(?:[\s-][a-zA-Z]+)*$/i)
+						if (!cityValid.value) {
+							fieldValidationErrors.city = cityValid.value ? '' : 'Invalid input'
+							break
+						}
+						cityValid.value = value.length > 0 
+						fieldValidationErrors.city = cityValid.value ? '' : 'Field cannot be empty'
+						break
+					case 'street':
+						value = value.trim();
+						streetValid.enable = true
+						streetValid.value = value.length > 0
+						fieldValidationErrors.street = streetValid.value ? '' : 'Field cannot be empty'
+						break
+					case 'numOfRooms':
+						value = value.trim()
+						numOfRoomsValid.enable = true
+						numOfRoomsValid.value = value.match(/^[1-9]\d*$/i)
+						fieldValidationErrors.numOfRooms = numOfRoomsValid.value ? '' : 'Invalid field'
+						break
+					case 'price':
+						value = value.trim()
+						priceValid.enable = true
+						priceValid.value = value.match(/^[1-9]\d*$/i)
+						fieldValidationErrors.price = priceValid.value ? '' : 'Invalid field'
+						break
+					case 'fee':
+						value = value.trim()
+						feeValid.enable = true
+						feeValid.value = value.match(/^[1-9]\d*$/i)
+						fieldValidationErrors.fee = feeValid.value ? '' : 'Invalid field'
+						break
+					case 'capacity':
+						value = value.trim()
+						capacityValid.enable = true
+						capacityValid.value = value.match(/^[1-9]\d*$/i)
+						fieldValidationErrors.capacity = capacityValid.value ? '' : 'Invalid field'
+						break
+					default:
+						break
+        }
+
+				this.setState({
+					formErrors: fieldValidationErrors,
+					cottageNameValid: cottageNameValid,
+					countryValid: countryValid,
+        	cityValid: cityValid,
+        	streetValid: streetValid,
+					numOfRoomsValid: numOfRoomsValid,
+					priceValid: priceValid,
+					feeValid: feeValid,
+					capacityValid: capacityValid,
+				}, this.validateForm)
     }
-}  
+
+		validateForm = () => {
+			this.setState({ formValid: this.state.cottageNameValid.value && this.state.countryValid.value && this.state.cityValid.value &&
+				this.state.streetValid.value && this.state.numOfRoomsValid.value && this.state.priceValid.value && this.state.feeValid.value && 
+				this.state.capacityValid.value })
+		}
+
+    render() {
+			const { cottageName, country, city, street, description, numOfRooms, capacity, regulations, price, fee, images, 
+				formErrors, cottageNameValid, streetValid, cityValid, countryValid, numOfRoomsValid, priceValid, capacityValid, feeValid, formValid } = this.state
+			const values = { cottageName, country, city, street, description, numOfRooms, capacity, regulations, price, fee, images }
+			const validation = { formErrors, cottageNameValid, countryValid, cityValid, streetValid, numOfRoomsValid, priceValid, capacityValid, feeValid, formValid }
+			
+			return(
+					<AddCottage
+							user = { this.props.user }
+							values = { values }
+							validation = { validation}
+							handleChange = {this.handleChange}
+							handleImages = {this.handleImages}
+					/>
+			)
+    }
+}
