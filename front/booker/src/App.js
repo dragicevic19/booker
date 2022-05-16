53d6199271b805a23ad6cdea8dba5b76499b25fd
Boki0 <bojan.vasic01@gmail.com>
2022-05-16 16:14:48 +0200

    izbrisan stari front

--- front/booker/src/App.js
+++ front/booker/src/App.js
@@ -1,11 +1,7 @@
 import Home from './components/Home';
 //import Navbar from './components/Navbar';
-import Login from './components/Login';
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom'; 
-import Registration from './components/registration/Registration';
 import { useState } from "react"
-import ShowMyProperty from './components/showMyProperty/ShowMyProperty';
-import NewCottage from './components/addCottage/NewCottage';
 import NavbarHome from './components/navbarHome/Navbar';
 
 
@@ -19,10 +15,10 @@ function App() {
         <div className="content">
           <Routes>
             <Route path="/" element= {<Home />} />
-            <Route path="/login" element = {<Login onLogin={setLoggedInUser}/>} />
+            {/* <Route path="/login" element = {<Login onLogin={setLoggedInUser}/>} />
             <Route path='/register' element = {<Registration />} />
             <Route path='/my-offers' element = {<ShowMyProperty user={loggedInUser} onLogin={setLoggedInUser}/>} />
-            <Route path='/add-cottage' element = {<NewCottage user={loggedInUser}/>} />
+            <Route path='/add-cottage' element = {<NewCottage user={loggedInUser}/>} /> */}
 
           </Routes>
         </div>
