e9f4c078dd3533a65eabff19356c56149e5931b9
Marko Dragicevic <mdragicevic58@gmail.com>
2022-06-15 16:42:28 +0200

    2.14 | 1 - Prikaz tabele sa istorijom rezervacija

---
+++ front/booker/src/dashboard/resColumns.js
@@ -0,0 +1,51 @@
+export const reservationColumns = 
+  [
+    { field: "id", headerName: "ID", width: 100 },
+    {
+      field: "offerName",
+      headerName: "Offer Name",
+      width: 250,
+      renderCell: (params) => {
+        return (
+          <div className="cellWithImg">
+            <img className="cellImg" src={params.row.img || "https://img.favpng.com/5/18/7/log-cabin-ico-icon-png-favpng-68ATTkwWVbt7jKvV6mmTS9mCs.jpg"} alt="avatar" />
+            {params.row.offerName}
+          </div>
+        );
+      }
+    },
+    {
+      field: "client",
+      headerName: "Client",
+      width: 250,
+      renderCell: (params) => {
+        return (
+          <div className="clientCellWithImg">
+            <img className="cellImg" src={params.row.clientImg || "https://cdn0.iconfinder.com/data/icons/set-ui-app-android/32/8-512.png"} alt="avatar" />
+            {params.row.client}
+          </div>
+        );
+      }
+    },
+    {
+      field: "city",
+      headerName: "City",
+      width: 150,
+    },
+    {
+      field: "price",
+      headerName: "Price [$]",
+      width: 100,
+    },
+    {
+      field: "capacity",
+      headerName: "Attendants",
+      width: 120,
+    },
+    {
+      field: "period",
+      headerName: "Period",
+      width: 230,
+    }
+  ]
+  
