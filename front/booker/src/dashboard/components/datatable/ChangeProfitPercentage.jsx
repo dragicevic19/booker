1f047f5138c09154d3e6cc510881f9f942d24cdd
Raffayet <soviljnikola3@gmail.com>
2022-06-21 11:43:52 +0200

    3.11.5

--- front/booker/src/dashboard/components/datatable/ChangeProfitPercentage.jsx
+++ front/booker/src/dashboard/components/datatable/ChangeProfitPercentage.jsx
@@ -9,26 +9,70 @@ import { AuthContext } from '../../../components/context/AuthContext';
 import { CircularProgressbar } from "react-circular-progressbar";
 import "react-circular-progressbar/dist/styles.css";
 import "./circularBar.scss"
+import useFetch from '../../../hooks/useFetch';
+import { useEffect } from 'react';
+import { useNotification } from '../../../components/notification/NotificationProvider';
 
 export default function ChangeProfitPercentage() {
-
+  
   const { user } = useContext(AuthContext);
 
   const [open, setOpen] = useState(false);
 
-  const [response, setResponse] = useState('');
+  const [profitPercentage, setProfitPercentage] = useState();
+
+  const { data, loading, error } = useFetch(`http://localhost:8080/api/get-profit-percentage`);
+
+  useEffect(() => {
+    setProfitPercentage(data);
+  }, [data]);
 
   const handleClickOpen = () => {
     setOpen(true);
   };
-
+  
   const handleClose = () => {
     setOpen(false);
   };
 
-  const handeChangeResponse = (e) =>
+  const handleChangeProfitPercentage = (e) =>
   {
-    setResponse(e.target.value)
+    if(e.target.value > 0 || e.target.value < 85)
+    {
+      setProfitPercentage(e.target.value)
+    }
+  }
+
+  const dispatch = useNotification();
+
+  const handleSubmit = (e) => {
+
+    if (![...Array(86).keys()].includes(parseInt(profitPercentage)))
+    {
+      sendNotification("error", "The value must be between 0 and 85")
+    }
+    else{
+      e.preventDefault()
+      fetch(`http://localhost:8080/api/change-profit-percentage?profitPercentageValue=${parseFloat(profitPercentage)/100}`, {
+        method: 'POST',
+        headers: 
+      {'Content-Type': 'application/json',
+        'Authorization': `Bearer ${user.accessToken}`,
+      },
+      })
+      .then(data => {
+        sendNotification("success", "You successfully sent a request for registration. Please wait for administrator to approve your request!");
+      })
+      handleClose();
+    }
+  }
+
+  const sendNotification = (type, message) => {
+    dispatch({
+      type: type,
+      message: message,
+      navigateTo: '/'
+    });
   }
 
   return (
@@ -43,16 +87,22 @@ export default function ChangeProfitPercentage() {
         <div className="circularBarWrapper">
           <div className="bottomCircularBar">
             <div className="circularBarItem">
-              <CircularProgressbar value={20} text={'20%'} strokeWidth={5} />
+              <CircularProgressbar value={profitPercentage} text={profitPercentage + '%'} strokeWidth={5} />
             </div>
             <p className="titleCircularBar">Profit Percentage</p>
-            <p className="amountCircularBar">{'df'}</p>
+             <input id='numberInput' className='changeProfitInput' type="number"
+             min="0" 
+             max="85"
+             value={profitPercentage}
+             onChange={handleChangeProfitPercentage}
+             defaultValue={profitPercentage}
+              />
           </div>
         </div>
         </DialogContent>
         <DialogActions>
-          <Button style={{color: '#0071c2'}} onClick={handleClose}>Cancel</Button>
-          <Button style={{color: '#0071c2'}} onClick={handleClose}>Submit</Button>
+          <Button style={{color: '#8B0000'}} onClick={handleClose}>Cancel</Button>
+          <Button style={{color: '#0071c2'}} onClick={handleSubmit}>Submit Change</Button>
         </DialogActions>
       </Dialog>
     </div>
